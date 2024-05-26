bf5effc18094a2e41a73c3fdb1d26d4a28352774
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2024-05-26 22:02:05 +0530

    made a bit of header

--- HungerBox/index.js
+++ HungerBox/index.js
@@ -1,19 +1,51 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-const Heading1=(
-    <h1 id="a" key="h1">
-        this is first heading
-    </h1>
-);
-const Heading2=()=>{
+const Logo=()=>{
+    return(
+        <img className="logo" src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent-2048x2048.png" alt="image"/>
+    )
+}
+
+const Header=()=>{
+    return(
+        <div id="header">
+            <Logo/>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    )
+}
+
+const Body=()=>{
     return (
-        <h1 id="b" key="h2">
-            this is second heading
-        </h1>
-    );
+        <div id="body">
+            <h1>Body</h1>
+        </div>
+    )
+}
+
+const Footer=()=>{
+    return (
+        <div id="footer">
+            <h1>Footer</h1>
+        </div>
+    )
+}
+
+const AppLayout=()=>{
+    return(
+        <React.Fragment>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </React.Fragment>
+    )
 };
-const root1=ReactDom.createRoot(document.getElementById('root1'));
-const root2=ReactDom.createRoot(document.getElementById('root2'));
-root1.render(Heading1);
-root2.render(<Heading2/>);
+
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
