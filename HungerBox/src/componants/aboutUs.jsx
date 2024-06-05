a8e5bbd6d918c604331c0cff0904ed142f808d6b
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2024-06-05 23:28:37 +0530

    minor changes to about us

--- HungerBox/src/componants/aboutUs.jsx
+++ HungerBox/src/componants/aboutUs.jsx
@@ -1,9 +1,39 @@
-import { Suspense } from "react";
-import ShimmerUI from "./shimmerUi";
+import { useState } from "react";
+
+const InfoCard=({header,information})=>{
+    let [showInfo,setShowInfo]=useState(false);
+
+    return (
+    <div className="infoCard">
+
+    <h1>{header}</h1>
+    {showInfo && <button onClick={()=>setShowInfo(false)}>Hide</button>}
+    {!showInfo && <button onClick={()=>setShowInfo(true)}>Show</button>}
+    {showInfo && <p>{information}</p>}
+
+    </div>
+    )
+}
 
 const AboutUs=()=>{
     return (   
-        <h1>Hi, i am Piyush Chowdhury</h1>
+        <div>
+        <InfoCard
+        key={1}
+        header={"header1"}
+        information={"information1"}
+        />
+        <InfoCard
+        key={2}
+        header={"header2"}
+        information={"information2"}
+        />
+        <InfoCard
+        key={3}
+        header={"header3"}
+        information={"information3"}
+        />
+        </div>
     )
 }
 export default AboutUs;
