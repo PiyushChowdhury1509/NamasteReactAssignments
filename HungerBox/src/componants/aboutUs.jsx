d268bcf07247c5e156381237db430cce1a053418
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2024-06-06 23:19:55 +0530

    added a feature to about us

--- HungerBox/src/componants/aboutUs.jsx
+++ HungerBox/src/componants/aboutUs.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
-const InfoCard=({header,information})=>{
-    let [showInfo,setShowInfo]=useState(false);
+const InfoCard=({header,information,showInfo,setShowInfo,hideInfo})=>{
 
     return (
     <div className="infoCard">
 
     <h1>{header}</h1>
-    {showInfo && <button onClick={()=>setShowInfo(false)}>Hide</button>}
-    {!showInfo && <button onClick={()=>setShowInfo(true)}>Show</button>}
+    {showInfo && <button onClick={()=>hideInfo()}>Hide</button>}
+    {!showInfo && <button onClick={()=>setShowInfo()}>Show</button>}
     {showInfo && <p>{information}</p>}
 
     </div>
@@ -16,22 +15,73 @@ const InfoCard=({header,information})=>{
 }
 
 const AboutUs=()=>{
+    let [configInfo,setConfigInfo]=useState({
+        player1:false,
+        player2:false,
+        player3:false,
+    });
+
     return (   
         <div>
         <InfoCard
         key={1}
         header={"header1"}
         information={"information1"}
+        showInfo={configInfo.player1}
+        setShowInfo={()=>{
+            setConfigInfo({
+                player1:true,
+                player2:false,
+                player3:false,
+            })
+        }}
+        hideInfo={()=>{
+            setConfigInfo({
+                player1:false,
+                player2:false,
+                player3:false,
+            })
+        }}
         />
         <InfoCard
         key={2}
         header={"header2"}
         information={"information2"}
+        showInfo={configInfo.player2}
+        setShowInfo={()=>{
+            setConfigInfo({
+                player1:false,
+                player2:true,
+                player3:false,
+            })
+        }}
+        hideInfo={()=>{
+            setConfigInfo({
+                player1:false,
+                player2:false,
+                player3:false,
+            })
+        }}
         />
         <InfoCard
         key={3}
         header={"header3"}
         information={"information3"}
+        showInfo={configInfo.player3}
+        setShowInfo={()=>{
+            setConfigInfo({
+                player1:false,
+                player2:false,
+                player3:true,
+            })
+        }}
+        hideInfo={()=>{
+            setConfigInfo({
+                player1:false,
+                player2:false,
+                player3:false,
+            })
+        }}
         />
         </div>
     )
